0ef526e1131dfd02879d382a6b8dc6230760e9b9
Luis <s.ros.lui@gmail.com>
2020-11-29 12:35:37 +0100

    add getAPI con Axios

--- main.js
+++ main.js
@@ -15,4 +15,27 @@
 
 
 
- console.log("JS FUNZIONA");
+//  console.log("JS FUNZIONA");
+const app = new Vue({
+    el: "#app",
+    data: {
+        arrayCD: []
+    },
+    created(){
+        // prende gli album
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music") // url dell' API
+            .then(result =>{
+                // caso di successo
+                // oppure .then(function(result){})
+                console.log(result.data);   //stampa oggetti result con le sue proprietà (success e response)
+
+                //ora, riempio l' arrayCD degli oggetti estratti dall' API
+                this.arrayCD = result.data.response;
+            })
+            .catch(error => {
+                // caso di errore
+                // oppure .catch(function(errore){})
+                console.log(error);
+            });
+    }
+});
