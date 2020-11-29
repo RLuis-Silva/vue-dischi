f8e6ceee2a181dc5b211e867a10a81ad299ff528
Luis <s.ros.lui@gmail.com>
2020-11-29 11:41:01 +0100

    add file readme + img

---
+++ README/README.md
@@ -0,0 +1,11 @@
+vue-dischi
+
+Esercizio di oggi: Vue dischi
+cartella/repo: vue-dischi
+Rieseguire il live coding visto insieme durante la lezione (giorno 24Nov video 58):
+Attraverso una chiamata AJAX all’API di boolean avremo a disposizione una decina di dischi musicali. 
+Utilizzare Postman per analizzare i dati restituiti dall'API, ancora prima di scrivere codice.
+Utilizzando Vue stampiamo tutto a schermo. 
+Creare poi una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+Endpoint chiamata API: https://flynn.boolean.careers/exercises/api/array/music
+Link download Postman: https://www.postman.com/downloads/
---
+++ main.js
@@ -0,0 +1,18 @@
+/**
+ * Rieseguire il live coding visto insieme durante la lezione (giorno 24Nov video 58)
+ * Attraverso una chiamata AJAX all' API di boolean avremo a disposizione una decina di dischi musicali.
+ * 
+ * utilizzare Postman per analizzare i dati restituiti dall' API, ancora prima di scrivere codice.
+ * 
+ * Utilizzando Vue stampiamo tutto a schermo.
+ * 
+ * Creare una select con i seguneti generi: pop, rock, metal e jazz.
+ * in base a cosa scegliamo nella select vedremo i corrispondenti cd.
+ * 
+ * Endpoint chiamata API: https://flynn.boolean.careers/exercises/api/array/music
+ * Link download Postman: https://www.postman.com/downloads/
+ */
+
+
+
+ console.log("JS FUNZIONA");
